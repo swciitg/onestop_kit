252b2687a767c7e137703cfe033e1c1e2263ca5e
Hardik Roongta <hardikroongta8@gmail.com>
2024-04-13 20:16:07 +0530

    minor change

--- lib/onestop_kit.dart
+++ lib/onestop_kit.dart
@@ -1,5 +1,6 @@
 library onestop_kit;
 
+export 'src/api/backend_helper.dart';
 export 'src/api/onestop_api.dart';
 export 'src/constants/onestop_colors.dart';
 export 'src/constants/onestop_styles.dart';
