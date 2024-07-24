617b8e89fabab944ed416dead18a51447413d97e
Arpit Sureka <surekaarpit.com>
2024-07-24 16:15:07 +0530

    minor fix

--- lib/src/ui/error_reload_button.dart
+++ lib/src/ui/error_reload_button.dart
@@ -2,35 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:onestop_kit/onestop_kit.dart';
 
 class ErrorReloadButton extends StatelessWidget {
-  final Function reloadCallback;
+  final void Function() reloadCallback;
 
   const ErrorReloadButton({required this.reloadCallback, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      child: ElevatedButton.icon(
-        onPressed: () {
-          reloadCallback();
-        },
-        icon: const Icon(
-          Icons.rotate_right,
-          size: 16,
-          color: OneStopColors.kBlack,
-        ),
-        label: const Text(
-          "Try again",
-          style: TextStyle(
-            color: OneStopColors.kBlack,
-            fontSize: 12,
-            fontWeight: FontWeight.w600,
+    return Flex(
+      direction: Axis.vertical,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: OneStopColors.kYellow,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: InkWell(
+            onTap: reloadCallback,
+            borderRadius: BorderRadius.circular(20),
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(
+                    Icons.rotate_right,
+                    size: 16,
+                    color: OneStopColors.kBlack,
+                  ),
+                  Text(
+                    "Try again",
+                    style: TextStyle(
+                      color: OneStopColors.kBlack,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
-        style: const ButtonStyle(
-          backgroundColor: WidgetStatePropertyAll(OneStopColors.kYellow),
-        ),
-      ),
+      ],
     );
   }
 }
