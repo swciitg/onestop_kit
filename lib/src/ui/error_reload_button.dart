6c90fe6ffa80c02dc539f8dc9f19802ddf59d6b2
Hardik Roongta <hardikroongta8@gmail.com>
2024-10-20 17:37:44 +0530

    minor change

--- lib/src/ui/error_reload_button.dart
+++ lib/src/ui/error_reload_button.dart
@@ -8,42 +8,37 @@ class ErrorReloadButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flex(
-      direction: Axis.vertical,
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: OneStopColors.kYellow,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: InkWell(
-            onTap: reloadCallback,
-            borderRadius: BorderRadius.circular(20),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.rotate_right,
-                    size: 16,
-                    color: OneStopColors.kBlack,
-                  ),
-                  Text(
-                    "Try again",
-                    style: TextStyle(
-                      color: OneStopColors.kBlack,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ],
+    return Container(
+      decoration: BoxDecoration(
+        color: OneStopColors.kYellow,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: InkWell(
+        onTap: reloadCallback,
+        borderRadius: BorderRadius.circular(20),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.rotate_right,
+                size: 16,
+                color: OneStopColors.kBlack,
               ),
-            ),
+              Text(
+                "Try again",
+                style: TextStyle(
+                  color: OneStopColors.kBlack,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
           ),
         ),
-      ],
+      ),
     );
   }
 }
